bdca119d9d1ffd9de8f1aead1689a90c31af55dd
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-31 23:37:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ end
 #    self.artist = artists_store[0]
 #end
 
-#end   Dont Repeat yourself functionality is extened from the above function  
+#end   Dont Repeat yourself functionality is extened from the above function
 
 def artist_name=(name)
   artists_store = Artist.all.select{|artist|artist.name===name}
