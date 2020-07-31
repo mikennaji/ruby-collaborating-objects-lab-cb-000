b3d3fdfa814b2ecf69b4c58b37614240aaf567ba
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-31 23:36:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,54 @@
-
 class Song
 
-  attr_accessor :name, :artist
+attr_accessor :name, :artist
 
-  @@all = []
+@@all = []
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-  @@all
-  end
-
-def self.new_by_filename(file)
-  song_info = file.chomp(".mp3").split(" - ")
-  song = Song.new(song_info[1])
-  song.artist_name = song_info[0]
-  song
+def initialize(name)
+  @name = name
+  save
 end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+def save
+    @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.new_by_filename(filename)
+  song = Song.new(filename.split(' - ')[1])
+  artist = Artist.new(filename.split(' - ')[0])
+  song.artist =artist
+  return song
+end
+
+#def artist_name=(name)
+# artists_store = Artist.all.select{|artist|artist.name===name}
+
+ #if artists_store.empty?
+##   artist= Artist.new(name)
+#   self.artist = artist
+#    self.artist = artists_store[0]
+#end
+
+#end   Dont Repeat yourself functionality is extened from the above function  
+
+def artist_name=(name)
+  artists_store = Artist.all.select{|artist|artist.name===name}
+
+   if artists_store.empty?
+     self.artist = Artist.find_or_create_by_name(name)
+     artist.add_song(self)
+   else
+     self.artist = artists_store[0]
+ end
+
+
+
+
+
+
 
 end
